2ccdba14cacb5e7fef59e730245c860a6c98dd2d
d-eniz <zcemdte@ucl.ac.uk>
2024-12-02 11:20:55 +0000

    Enhance docstring for tree function to include parameters and improve clarity

--- trees/trees.py
+++ trees/trees.py
@@ -1,15 +1,19 @@
-"""
-This code produces a tree like plot. In order to achieve this we plot an initial
-line and present the roots/trunk of the tree, after which we produce a loop to
-set the x and y values. This then plots the points on a graph through matplotlib
-which produces a graph similar to the shape of a tree.
-"""
-
 from math import sin, cos
 from matplotlib import pyplot as plt
 
 
 def tree(branch_length=1, branch_angle=0.1, num_branches=5):
+    """
+    This function produces a tree like plot. In order to achieve this we plot an initial
+    line and present the roots/trunk of the tree, after which we produce a loop to
+    set the x and y values. This then plots the points on a graph through matplotlib
+    which produces a graph similar to the shape of a tree.
+
+    Parameters:
+        branch_length (float): The length of the branches of the tree.
+        branch_angle (float): The angle between the branches of the tree.
+        num_branches (int): The number of branches the tree has.
+    """
     branches = [
         [0, 1, 0]
     ]  # list of branches, each branch is a list of 3 numbers: x, y, angle
