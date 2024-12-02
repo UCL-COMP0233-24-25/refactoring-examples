41dbd6beee410bfffddbca43ad85490763dc0eb8
Luke Amos <luke.amos.24@ucl.ac.uk>
2024-12-02 11:11:02 +0000

    Changing sum_weighted_diffs to use enumerate in iteration.

--- analysis/utils.py
+++ analysis/utils.py
@@ -14,8 +14,7 @@ def read_file(filename):
 def sum_weighted_diffs(data1, data2, w):
     """Calculates sum of weighted differences between items in data1 and data2"""
     results = []
-    for i in range(len(data1)):
-        row1 = data1[i]
+    for i, row1 in enumerate(data1):
         row2 = data2[i]
         diffs = np.abs(row1-row2)
         sum_weighted_diffs = np.sum(diffs*w)
