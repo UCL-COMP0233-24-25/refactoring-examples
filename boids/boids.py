11da0cbb23b97e2dc94dc06846282816547525a2
sanli <ucaphlu@ucl.ac.uk>
2024-12-02 10:40:59 +0000

    boids.py add explaining comments for universal variables

--- boids/boids.py
+++ boids/boids.py
@@ -10,10 +10,14 @@ from matplotlib import animation
 
 import random
 
+
+
+# these variables are lists of random integers
 boids_x=[random.uniform(-450,50.0) for x in range(50)]
 boids_y=[random.uniform(300.0,600.0) for x in range(50)]
 boid_x_velocities=[random.uniform(0,10.0) for x in range(50)]
 boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(50)]
+# boids is the collection of the lists
 boids=(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
 
 def update_boids(boids):
