4d50517b70273f9c0b94ec477181b0645e482580
d-eniz <zcemdte@ucl.ac.uk>
2024-12-02 10:48:49 +0000

    Enhance docstring for tree plotting function and improve inline comments

--- trees/trees.py
+++ trees/trees.py
@@ -1,4 +1,9 @@
-"""This code produces a tree-like plot through matplotlib."""
+"""
+This code produces a tree like plot. In order to achieve this we plot an initial
+line and present the roots/trunk of the tree, after which we produce a loop to
+set the x and y values. This then plots the points on a graph through matplotlib
+which produces a graph similar to the shape of a tree.
+"""
 
 from math import sin, cos
 from matplotlib import pyplot as plt
@@ -7,7 +12,7 @@ branch_length = 1  # length of the first branch
 branches = [
     [0, 1, 0]
 ]  # list of branches, each branch is a list of 3 numbers: x, y, angle
-plt.plot([0, 0], [0, 1])  #
+plt.plot([0, 0], [0, 1])  # plots the initial line of the tree
 for i in range(5):
     new_branches = []
     for branch in branches:  # Loop over existing branches
@@ -29,8 +34,8 @@ for i in range(5):
         # Plot the new branches
         plt.plot([x, new_x1], [y, new_y1])
         plt.plot([x, new_x2], [y, new_y2])
-        
+
     branches = new_branches
     branch_length *= 0.6
-    
+
 plt.savefig("tree.png")
