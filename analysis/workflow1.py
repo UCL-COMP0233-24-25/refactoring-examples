d68bb8070deab86772a05a73471dddc783aaa8fa
Luke Amos <luke.amos.24@ucl.ac.uk>
2024-12-02 10:42:15 +0000

    Wrapping script in if __name__=="main" statement.

--- analysis/workflow1.py
+++ analysis/workflow1.py
@@ -12,23 +12,25 @@ def read_file(filename):
             data.append(row)
     return data
 
-data1 = read_file("data1.csv")
-data2 = read_file("data2.csv")
-w = read_file("weights.csv")[0]
 
-results = []
-for i in range(len(data1)):
-    s = 0
-    for j in range(len(w)):
-        d = data1[i][j] - data2[i][j]
-        s += w[j] * abs(d)
-    results.append(s)
+if __name__ == "__main__":
+    data1 = read_file("data1.csv")
+    data2 = read_file("data2.csv")
+    w = read_file("weights.csv")[0]
 
-critical = 0
-for i in range(len(results)):  # for all i
-    if results[i] > 5:
-        critical = critical + 1  # increase by 1
-if critical == 1:
-    print("criticality: 1 result above 5")
-else:
-    print("criticality:", critical, "results above 5")
+    results = []
+    for i in range(len(data1)):
+        s = 0
+        for j in range(len(w)):
+            d = data1[i][j] - data2[i][j]
+            s += w[j] * abs(d)
+        results.append(s)
+
+    critical = 0
+    for i in range(len(results)):  # for all i
+        if results[i] > 5:
+            critical = critical + 1  # increase by 1
+    if critical == 1:
+        print("criticality: 1 result above 5")
+    else:
+        print("criticality:", critical, "results above 5")
