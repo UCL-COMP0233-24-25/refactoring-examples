df0b10caf3a84a0d06a6659bd6b4f748cccc1dd7
Shyam Modi <zcabsjm@ucl.ac.uk>
2024-12-02 10:59:43 +0000

    refactored to loops to iterators using zip

--- analysis/workflow1.py
+++ analysis/workflow1.py
@@ -2,37 +2,40 @@ from math import *
 
 # read sample files
 
+
 def read_file(csv):
     with open(csv) as file:
         lines = file.readlines()
         data = []
         for line in lines:
             row = []
-            for n in line.split(','):
+            for n in line.split(","):
                 row.append(float(n.strip()))
             data.append(row)
     return data
 
-data1 = read_file('analysis/data1.csv')
-data2 = read_file('analysis/data2.csv')
 
-with open('analysis/weights.csv') as filew:
+data1 = read_file("analysis/data1.csv")
+data2 = read_file("analysis/data2.csv")
+
+with open("analysis/weights.csv") as filew:
     linew = filew.read()
     w = []
-    for n in linew.split(','):
+    for n in linew.split(","):
         w.append(float(n.strip()))
 
 results = []
-for i in range(len(data1)):
+for row1, row2 in zip(data1, data2):
     s = 0
-    for j in range(len(w)):
-        d = data1[i][j] - data2[i][j]
-        s += w[j] * abs(d)
+    for item in range(len(row1)):
+        d = row1[item] - row2[item]
+        s += w[item] * abs(d)
     results.append(s)
 
+
 critical = 0
-for i in range(len(results)):  # for all i
-    if results[i] > 5:
+for item in results:  # for all i
+    if item > 5:
         critical = critical + 1  # increase by 1
 if critical == 1:
     print("criticality: 1 result above 5")
