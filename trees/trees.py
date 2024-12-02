d902f65f1ac5fba37afdba2f3165876a1c6f0085
echolmy <echoliumy@outlook.com>
2024-12-02 11:15:49 +0000

    seperate loop of calcaulating trees and plot into helper functions

--- trees/trees.py
+++ trees/trees.py
@@ -3,35 +3,61 @@
 from math import sin, cos
 from matplotlib import pyplot as plt
 
+
+def compute_coordinate(coord, length, left=True):
+    """calculate the coordinate
+
+    Args:
+        coord (list): current coordinate store x, y and angle
+        length (float): length of line at this level
+        left (bool, optional): calculate the coordinate and angle according to the left branch or right branch. Defaults to True.
+
+    Returns:
+        list: next coordinate
+    """
+    angle_dif = -0.1 if left else 0.1
+
+    next_coord_x = coord[0] + length * sin(coord[2] + angle_dif)
+    next_coord_y = coord[1] + length * cos(coord[2] + angle_dif)
+    next_coord_angle = coord[2] + angle_dif
+
+    return [next_coord_x, next_coord_y, next_coord_angle]
+
+
+def calculate_next_cordinates(coordinate, length):
+    """caculate coordinates and length
+
+    Args:
+        coordinate (list): current coordinate store x, y and angle
+        length (float): length of line at this level
+
+    Returns:
+        _type_: next coordinate, next length
+    """
+    next_cordinates = []
+    for coord in coordinate:  # loop over coordinate
+        next_cordinates.append(compute_coordinate(coord, length, True))
+        next_cordinates.append(compute_coordinate(coord, length, False))
+        plt.plot(
+            [coord[0], next_cordinates[-2][0]],
+            [coord[1], next_cordinates[-2][1]],
+        )
+        plt.plot(
+            [coord[0], next_cordinates[-1][0]],
+            [coord[1], next_cordinates[-1][1]],
+        )
+    coordinate = next_cordinates
+    length *= 0.6
+
+    return coordinate, length
+
+
 length = 1
 #  (0: x, 1 :y, 0: angle )
 coordinate = [[0, 1, 0]]
 plt.plot([0, 0], [0, 1])
+
 for i in range(5):
-    next_cordinates = []
-    for j in range(len(coordinate)):  # loop over d
-        next_cordinates.append(
-            [
-                coordinate[j][0] + length * sin(coordinate[j][2] - 0.1),
-                coordinate[j][1] + length * cos(coordinate[j][2] - 0.1),
-                coordinate[j][2] - 0.1,
-            ]
-        )
-        next_cordinates.append(
-            [
-                coordinate[j][0] + length * sin(coordinate[j][2] + 0.1),
-                coordinate[j][1] + length * cos(coordinate[j][2] + 0.1),
-                coordinate[j][2] + 0.1,
-            ]
-        )
-        plt.plot(
-            [coordinate[j][0], next_cordinates[-2][0]],
-            [coordinate[j][1], next_cordinates[-2][1]],
-        )
-        plt.plot(
-            [coordinate[j][0], next_cordinates[-1][0]],
-            [coordinate[j][1], next_cordinates[-1][1]],
-        )
-    coordinate = next_cordinates
-    length *= 0.6
+    coordinate, length = calculate_next_cordinates(coordinate, length)
+
 plt.savefig("tree.png")
