f6780667d88ffbdf7416bfe8295b2fe9584e4264
Crows12138 <782990295@qq.com>
2024-12-02 11:12:18 +0000

    combine the redundant loop

--- boids/boids.py
+++ boids/boids.py
@@ -23,24 +23,15 @@ def update_boids(boids, middle_param=0.01, nearby_away = 100, nearby_match_speed
     for i in x_positions:
         for j in x_positions:
             x_velocities[i]=x_velocities[i]+(x_positions[j]-x_positions[i])*middle_param/len(x_positions)
-    for i in x_positions:
-        for j in x_positions:
             y_velocities[i]=y_velocities[i]+(y_positions[j]-y_positions[i])*middle_param/len(x_positions)
-    # Fly away from nearby boids
-    
-    for i in x_positions:
-        for j in x_positions:
+    # Fly away from nearby boids      
             if (x_positions[j]-x_positions[i])**2 + (y_positions[j]-y_positions[i])**2 < nearby_away:
                 x_velocities[i]=x_velocities[i]+(x_positions[i]-x_positions[j])
                 y_velocities[i]=y_velocities[i]+(y_positions[i]-y_positions[j])
     # Try to match speed with nearby boids
-    
-    for i in x_positions:
-        for j in x_positions:
             if (x_positions[j]-x_positions[i])**2 + (y_positions[j]-y_positions[i])**2 < nearby_match_speed:
                 x_velocities[i]=x_velocities[i]+(x_velocities[j]-x_velocities[i])*0.125/len(x_positions)
                 y_velocities[i]=y_velocities[i]+(y_velocities[j]-y_velocities[i])*0.125/len(x_positions)
     # Move according to velocities
-    for i in x_positions:
         x_positions[i]=x_positions[i]+x_velocities[i]
         y_positions[i]=y_positions[i]+y_velocities[i]
