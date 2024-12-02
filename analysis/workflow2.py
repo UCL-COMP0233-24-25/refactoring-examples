d4f0f455e12f6ca5966e3cb8e54f6728d6690156
Luke Amos <luke.amos.24@ucl.ac.uk>
2024-12-02 11:02:42 +0000

    Wrapping in if __name__ == "__main__".

--- analysis/workflow2.py
+++ analysis/workflow2.py
@@ -1,20 +1,11 @@
 from utils import read_file, sum_weighted_diffs
 
-data1 = read_file("samples1.csv")
-data2 = read_file("samples2.csv")
-w = read_file("weights.csv")[0]
-
-results = sum_weighted_diffs(data1, data2, w)
-
-# critical = 0
-# for i in range(len(results)):
-#     if results[i] > 5:
-#         critical = critical + 1
-# if critical == 1:
-#     print("criticality: 1 result over 5")
-# else:
-#     print("criticality:", critical, "results over 5")
-dsum =  0
-for i in range(len(results)):
-    dsum = dsum + results[i]
-print("d-index:", dsum/len(results))
+if __name__ == "__main__":
+    data1 = read_file("samples1.csv")
+    data2 = read_file("samples2.csv")
+    w = read_file("weights.csv")[0]
+    results = sum_weighted_diffs(data1, data2, w)
+    dsum =  0
+    for i in range(len(results)):
+        dsum = dsum + results[i]
+    print("d-index:", dsum/len(results))
