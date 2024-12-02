ef3f2db19080666c04b6f2a7f90cd5b481bbe6a7
d-eniz <zcemdte@ucl.ac.uk>
2024-12-02 10:40:59 +0000

    Clarify variable names

--- trees/trees.py
+++ trees/trees.py
@@ -1,17 +1,36 @@
-"""This code produces a tree-like plot."""
+"""This code produces a tree-like plot through matplotlib."""
 
 from math import sin, cos
 from matplotlib import pyplot as plt
-s=1
-d=[[0,1,0]]
-plt.plot([0,0],[0,1])
+
+branch_length = 1  # length of the first branch
+branches = [
+    [0, 1, 0]
+]  # list of branches, each branch is a list of 3 numbers: x, y, angle
+plt.plot([0, 0], [0, 1])  #
 for i in range(5):
-    n=[]
-    for j in range(len(d)): #loop over d
-        n.append([d[j][0]+s*sin(d[j][2]-0.1), d[j][1]+s*cos(d[j][2]-0.1), d[j][2]-0.1])
-        n.append([d[j][0]+s*sin(d[j][2]+0.1), d[j][1]+s*cos(d[j][2]+0.1), d[j][2]+0.1])
-        plt.plot([d[j][0], n[-2][0]],[d[j][1], n[-2][1]])
-        plt.plot([d[j][0], n[-1][0]],[d[j][1], n[-1][1]])
-    d=n
-    s*=0.6
-plt.savefig('tree.png')
+    new_branches = []
+    for j in range(len(branches)):  # loop over d
+        new_branches.append(
+            [
+                branches[j][0] + branch_length * sin(branches[j][2] - 0.1),
+                branches[j][1] + branch_length * cos(branches[j][2] - 0.1),
+                branches[j][2] - 0.1,
+            ]
+        )
+        new_branches.append(
+            [
+                branches[j][0] + branch_length * sin(branches[j][2] + 0.1),
+                branches[j][1] + branch_length * cos(branches[j][2] + 0.1),
+                branches[j][2] + 0.1,
+            ]
+        )
+        plt.plot(
+            [branches[j][0], new_branches[-2][0]], [branches[j][1], new_branches[-2][1]]
+        )
+        plt.plot(
+            [branches[j][0], new_branches[-1][0]], [branches[j][1], new_branches[-1][1]]
+        )
+    d = new_branches
+    branch_length *= 0.6
+plt.savefig("tree.png")
