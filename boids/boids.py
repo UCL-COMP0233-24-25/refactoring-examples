b75ef3eacca7e7aa7809c14ec4aa14c51a69390f
AngelosZou <yutong.zou.24@ucl.ac.uk>
2024-12-02 10:57:58 +0000

    refactor boids to dataclass

--- boids/boids.py
+++ boids/boids.py
@@ -4,39 +4,49 @@ A deliberately bad implementation of
 for use as an exercise on refactoring.
 This code simulates the swarming behaviour of bird-like objects ("boids").
 """
+from dataclasses import dataclass
 
 import numpy as np
 
-def update_boids(boids_x,boids_y,boid_x_velocities,boid_y_velocities,
+@dataclass
+class Boids:
+    x: float
+    y: float
+    velocity_x: float
+    velocity_y: float
+
+def update_boids(boids_lst: list[Boids],
                  towards_middle_coefficient=0.01, fly_away_distance=100, nearby_distance=10000,
                  nearby_coefficient=0.125):
     # Fly towards the middle
-    for i in range(len(boids_x)):
-        for j in range(len(boids_x)):
-            boid_x_velocities[i]=boid_x_velocities[i]+(boids_x[j]-boids_x[i])*towards_middle_coefficient/len(boids_x)
-    for i in range(len(boids_x)):
-        for j in range(len(boids_x)):
-            boid_y_velocities[i]=boid_y_velocities[i]+(boids_y[j]-boids_y[i])*towards_middle_coefficient/len(boids_x)
+    for boid in boids_lst:
+        for other_boid in boids_lst:
+            boid.velocity_x = boid.velocity_x + (other_boid.x- boid.x)*towards_middle_coefficient/len(boids_lst)
+            boid.velocity_y = boid.velocity_y + (other_boid.y - boid.y)*towards_middle_coefficient/len(boids_lst)
     # Fly away from nearby boids
-    for i in range(len(boids_x)):
-        for j in range(len(boids_x)):
-            if (boids_x[j]-boids_x[i])**2 + (boids_y[j]-boids_y[i])**2 < fly_away_distance:
-                boid_x_velocities[i]=boid_x_velocities[i]+(boids_x[i]-boids_x[j])
-                boid_y_velocities[i]=boid_y_velocities[i]+(boids_y[i]-boids_y[j])
+    for boid in boids_lst:
+        for other_boid in boids_lst:
+            if (other_boid.x - boid.x)**2 + (other_boid.y - boid.y)**2 < fly_away_distance:
+                boid.velocity_x += boid.x - other_boid.x
+                boid.velocity_y += boid.y - other_boid.y
     # Try to match speed with nearby boids
-    for i in range(len(boids_x)):
-        for j in range(len(boids_x)):
-            if (boids_x[j]-boids_x[i])**2 + (boids_y[j]-boids_y[i])**2 < nearby_distance:
-                boid_x_velocities[i]=boid_x_velocities[i]+(boid_x_velocities[j]-boid_x_velocities[i])*nearby_coefficient/len(boids_x)
-                boid_y_velocities[i]=boid_y_velocities[i]+(boid_y_velocities[j]-boid_y_velocities[i])*nearby_coefficient/len(boids_x)
+    for boid in boids_lst:
+        for other_boid in boids_lst:
+            if (other_boid.x - boid.x)**2 + (other_boid.y - boid.y)**2 < nearby_distance:
+                boid.velocity_x += (other_boid.velocity_x - boid.velocity_x)*nearby_coefficient/len(boids_lst)
+                boid.velocity_y += (other_boid.velocity_y - boid.velocity_y)*nearby_coefficient/len(boids_lst)
     # Move according to velocities
-    for i in range(len(boids_x)):
-        boids_x[i]=boids_x[i]+boid_x_velocities[i]
-        boids_y[i]=boids_y[i]+boid_y_velocities[i]
+    for boid in boids_lst:
+        boid.x += boid.velocity_x
+        boid.y += boid.velocity_y
 
 if __name__ == "__main__":
-    boids_x=np.random.uniform(-450, 50., size=50)
-    boids_y=np.random.uniform(300, 600., size = 50)
-    boid_x_velocities=np.random.uniform(0,10, size=50)
-    boid_y_velocities=np.random.uniform(-20,20,size=50)
-    update_boids(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
+    population = 50
+    boids_lst = []
+    x=np.random.uniform(-450, 50., size=50)
+    y=np.random.uniform(300, 600., size = 50)
+    velocity_x=np.random.uniform(0, 10, size=50)
+    velocity_y=np.random.uniform(-20, 20, size=50)
+    for i in range(population):
+        boids_lst.append(Boids(x[i], y[i], velocity_x[i], velocity_y[i]))
+    update_boids(boids_lst)
