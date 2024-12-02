27c9a12c7fe5d7abd629d895e57a3dc62e1d5183
d-eniz <zcemdte@ucl.ac.uk>
2024-12-02 11:19:21 +0000

    Refactor tree function to accept parameters for branch length and angle, enhancing flexibility and readability

--- trees/trees.py
+++ trees/trees.py
@@ -8,34 +8,26 @@ which produces a graph similar to the shape of a tree.
 from math import sin, cos
 from matplotlib import pyplot as plt
 
-branch_length = 1  # length of the first branch
-branches = [
-    [0, 1, 0]
-]  # list of branches, each branch is a list of 3 numbers: x, y, angle
-plt.plot([0, 0], [0, 1])  #
-for i in range(5):
-    new_branches = []
-    for branch in branches:  # Loop over existing branches
-        x, y, angle = branch
 
-        # Calculate new branch points
-        new_x1 = x + branch_length * sin(angle - angle)
-        new_y1 = y + branch_length * cos(angle - angle)
-        new_angle1 = angle - angle
+def tree(branch_length=1, branch_angle=0.1, num_branches=5):
+    branches = [
+        [0, 1, 0]
+    ]  # list of branches, each branch is a list of 3 numbers: x, y, angle
+    plt.plot([0, 0], [0, 1])  # plot the trunk of the tree
+    for _ in range(num_branches):
+        new_branches = []  # Create a new list of branches
+        for branch in branches:
+            x, y, angle = branch
+            for new_angle in [angle - branch_angle, angle + branch_angle]:
+                new_x = x + branch_length * sin(new_angle)
+                new_y = y + branch_length * cos(new_angle)
+                new_branches.append([new_x, new_y, new_angle])
+                plt.plot([x, new_x], [y, new_y])
 
-        new_x2 = x + branch_length * sin(angle + angle)
-        new_y2 = y + branch_length * cos(angle + angle)
-        new_angle2 = angle + angle
+        branches = new_branches
+        branch_length *= 0.6
 
-        # Append new branches to the list
-        new_branches.append([new_x1, new_y1, new_angle1])
-        new_branches.append([new_x2, new_y2, new_angle2])
 
-        # Plot the new branches
-        plt.plot([x, new_x1], [y, new_y1])
-        plt.plot([x, new_x2], [y, new_y2])
-        
-    branches = new_branches
-    branch_length *= 0.6
-    
-plt.savefig("tree.png")
+if __name__ == "__main__":
+    tree()
+    plt.savefig("tree.png")
